a864a02e874bcacc83bc7a0cc764346c525301fd
catalina <catalinamenendezmendez@gmail.com>
2026-05-16 20:10:04 +0200

    TS-1: inicializar estructura de datos con Taylor's Version y Showgirl y configurar Firebase

---
+++ src/data/songs.js
@@ -0,0 +1,37 @@
+// src/data/songs.js
+
+export const albumsData = {
+  "Debut": { color: "#c6e2ca", text: "#2c4c38", isTV: false },
+  "Fearless (TV)": { color: "#f5e3b5", text: "#5c4308", isTV: true },
+  "Speak Now (TV)": { color: "#e1bee7", text: "#4a148c", isTV: true },
+  "Red (TV)": { color: "#ffcdd2", text: "#b71c1c", isTV: true },
+  "1989 (TV)": { color: "#e0f7fa", text: "#006064", isTV: true },
+  "Reputation": { color: "#cfd8dc", text: "#263238", isTV: false },
+  "Lover": { color: "#f8bbd0", text: "#880e4f" , isTV: false },
+  "Folklore": { color: "#f5f5f5", text: "#424242", isTV: false },
+  "Evermore": { color: "#ffe0b2", text: "#e65100", isTV: false },
+  "Midnights": { color: "#c5cae9", text: "#1a237e", isTV: false },
+  "TTPD": { color: "#eceff1", text: "#37474f", isTV: false },
+  "The Life of a Showgirl": { color: "#cca6b3", text: "#3d1d28", isTV: false }
+};
+
+export const songsList = [
+  { id: 1, name: "Our Song", album: "Debut" },
+  { id: 2, name: "Teardrops on My Guitar", album: "Debut" },
+  { id: 3, name: "Love Story", album: "Fearless (TV)" },
+  { id: 4, name: "You Belong With Me", album: "Fearless (TV)" },
+  { id: 5, name: "Mine", album: "Speak Now (TV)" },
+  { id: 6, name: "Enchanted", album: "Speak Now (TV)" },
+  { id: 7, name: "All Too Well (10 Min)", album: "Red (TV)" },
+  { id: 8, name: "Blank Space", album: "1989 (TV)" },
+  { id: 9, name: "Style", album: "1989 (TV)" },
+  { id: 10, name: "Delicate", album: "Reputation" },
+  { id: 11, name: "Cruel Summer", album: "Lover" },
+  { id: 12, name: "Lover", album: "Lover" },
+  { id: 13, name: "Cardigan", album: "Folklore" },
+  { id: 14, name: "Willow", album: "Evermore" },
+  { id: 15, name: "Anti-Hero", album: "Midnights" },
+  { id: 16, name: "Fortnight", album: "TTPD" },
+  { id: 17, name: "Behind the Curtain", album: "The Life of a Showgirl" },
+  { id: 18, name: "Velvet Lashes", album: "The Life of a Showgirl" }
+];
